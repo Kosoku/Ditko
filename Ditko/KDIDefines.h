e4e31a44841f2b0d718ef767a2a1c11bacb86231
William Towe <willbur1984@gmail.com>
2017-03-10 15:18:00 -0500

    added image specific URL extensions

--- Ditko/KDIDefines.h
+++ Ditko/KDIDefines.h
@@ -1,8 +1,8 @@
 //
-//  Ditko.h
+//  KDIDefines.h
 //  Ditko
 //
-//  Created by William Towe on 3/8/17.
+//  Created by William Towe on 3/10/17.
 //  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -13,31 +13,21 @@
 //
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#ifndef __KDI_DEFINES__
+#define __KDI_DEFINES__
+
 #import <TargetConditionals.h>
 
 #if (TARGET_OS_IPHONE)
 #import <UIKit/UIKit.h>
+
+#define KDIColor UIColor
+#define KDIImage UIImage
 #else
 #import <AppKit/AppKit.h>
+
+#define KDIColor NSColor
+#define KDIImage NSImage
 #endif
 
-//! Project version number for Ditko.
-FOUNDATION_EXPORT double DitkoVersionNumber;
-
-//! Project version string for Ditko.
-FOUNDATION_EXPORT const unsigned char DitkoVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Ditko/PublicHeader.h>
-
-#import <Ditko/KDIColorMacros.h>
-
-#if (TARGET_OS_IOS || TARGET_OS_OSX)
-#import <Ditko/KDIFunctions.h>
-#endif
-
-#import <Ditko/NSURL+KDIExtensions.h>
-
-#if (TARGET_OS_IOS || TARGET_OS_TV || TARGET_OS_OSX)
-#import <Ditko/KDIView.h>
-#import <Ditko/KDIGradientView.h>
 #endif
--- Ditko/NSURL+KDIExtensions.m
+++ Ditko/NSURL+KDIExtensions.m
@@ -1,8 +1,8 @@
 //
-//  Ditko.h
+//  NSURL+KDIExtensions.m
 //  Ditko
 //
-//  Created by William Towe on 3/8/17.
+//  Created by William Towe on 3/10/17.
 //  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -13,31 +13,16 @@
 //
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#import <TargetConditionals.h>
+#import "NSURL+KDIExtensions.h"
 
-#if (TARGET_OS_IPHONE)
-#import <UIKit/UIKit.h>
-#else
-#import <AppKit/AppKit.h>
-#endif
+@implementation NSURL (KDIExtensions)
 
-//! Project version number for Ditko.
-FOUNDATION_EXPORT double DitkoVersionNumber;
+- (KDIImage *)KDI_effectiveIcon {
+    KDIImage *retval = nil;
+    
+    [self getResourceValue:&retval forKey:NSURLEffectiveIconKey error:NULL];
+    
+    return retval;
+}
 
-//! Project version string for Ditko.
-FOUNDATION_EXPORT const unsigned char DitkoVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Ditko/PublicHeader.h>
-
-#import <Ditko/KDIColorMacros.h>
-
-#if (TARGET_OS_IOS || TARGET_OS_OSX)
-#import <Ditko/KDIFunctions.h>
-#endif
-
-#import <Ditko/NSURL+KDIExtensions.h>
-
-#if (TARGET_OS_IOS || TARGET_OS_TV || TARGET_OS_OSX)
-#import <Ditko/KDIView.h>
-#import <Ditko/KDIGradientView.h>
-#endif
+@end
