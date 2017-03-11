03686f96331bb07f6ecfe7a5b85cad2f65c59867
William Towe <willbur1984@gmail.com>
2017-03-10 19:58:36 -0500

    added KDITextField

--- Ditko/Ditko.h
+++ Ditko/Ditko.h
@@ -61,4 +61,5 @@ FOUNDATION_EXPORT const unsigned char DitkoVersionString[];
 #endif
 #if (TARGET_OS_IOS || TARGET_OS_TV)
 #import <Ditko/KDILabel.h>
+#import <Ditko/KDITextField.h>
 #endif
---
+++ Ditko/iOS/KDITextField.h
@@ -0,0 +1,47 @@
+//
+//  KDITextField.h
+//  Ditko
+//
+//  Created by William Towe on 3/10/17.
+//  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ KDITextField is a UITextField subclass that adds edge insets related methods to control the placement of text. This eliminates the need to use padding views as the left and right views of the receiver.
+ */
+@interface KDITextField : UITextField
+
+/**
+ Set and get the text edge insets of the receiver. This value affects the return values of textRectForBounds: and editingRectForBounds:.
+ 
+ The default is UIEdgeInsetsZero.
+ */
+@property (assign,nonatomic) UIEdgeInsets textEdgeInsets UI_APPEARANCE_SELECTOR;
+
+/**
+ Set and get the left view edge insets of the receiver. This value affects the return value of leftViewRectForBounds:.
+ 
+ The default is UIEdgeInsetsZero.
+ */
+@property (assign,nonatomic) UIEdgeInsets leftViewEdgeInsets UI_APPEARANCE_SELECTOR;
+/**
+ Set and get the right view edge insets of the receiver. This value affects the return value of rightViewRectForBounds:.
+ 
+ The default is UIEdgeInsetsZero.
+ */
+@property (assign,nonatomic) UIEdgeInsets rightViewEdgeInsets UI_APPEARANCE_SELECTOR;
+
+@end
+
+NS_ASSUME_NONNULL_END
