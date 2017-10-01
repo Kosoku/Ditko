439be437fd03845d244bf594ba95bbe9af4bbd5d
William Towe <willbur1984@gmail.com>
2017-10-01 15:38:05 -0400

    move strings to imp file

--- Ditko/iOS/KDIUIResponder.m
+++ Ditko/iOS/KDIUIResponder.m
@@ -1,8 +1,8 @@
 //
-//  KDIUIResponder.h
+//  KDIUIResponder.m
 //  Ditko
 //
-//  Created by William Towe on 9/2/17.
+//  Created by William Towe on 10/1/17.
 //  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -13,21 +13,7 @@
 //
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#import <Foundation/Foundation.h>
+#import "KDIUIResponder.h"
 
-/**
- Notification posted when an object becomes first responder. The object of the notification should be the object that just became first responder.
- */
-FOUNDATION_EXTERN NSNotificationName const KDIUIResponderNotificationDidBecomeFirstResponder;
-/**
- Notification posted when an object resigns first responder. The object of the notification should be the object that just resigned first responder.
- */
-FOUNDATION_EXTERN NSNotificationName const KDIUIResponderNotificationDidResignFirstResponder;
-
-@protocol KDIUIResponder <NSObject>
-@required
-/**
- Method is called when the first responder status of the receiver changes.
- */
-- (void)firstResponderDidChange;
-@end
+NSNotificationName const KDIUIResponderNotificationDidBecomeFirstResponder = @"KDIUIResponderNotificationDidBecomeFirstResponder";
+NSNotificationName const KDIUIResponderNotificationDidResignFirstResponder = @"KDIUIResponderNotificationDidResignFirstResponder";
