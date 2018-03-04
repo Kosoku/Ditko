fcb6b1969f3e75be1845f41cc877399aff7f91da
William Towe <willbur1984@gmail.com>
2018-03-04 15:38:35 -0500

    comments

--- Ditko/iOS/KDITableViewCell.h
+++ Ditko/iOS/KDITableViewCell.h
@@ -17,27 +17,105 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ KDITableViewCell is a UITableViewCell subclass that can display generic properties that can be used in a variety of situations. The layout is similar to Settings type cells, image aligned to the leading edge, title and subtitle after it stacked vertically and info aligned to the trailing edge. Internally the layout uses multiple stack views, so setting any of the value properties to nil will hide the corresponding subviews.
+ */
 @interface KDITableViewCell : UITableViewCell
 
+/**
+ Set and get whether the receiver should show its selected state using its accessoryType property. If YES, it will set its selectionStyle to UITableViewCellSelectionStyleNone and its accessoryType to UITableViewCellAccessoryCheckmark when selected or UITableViewCellAccessoryNone otherwise.
+ 
+ The default is NO.
+ */
 @property (assign,nonatomic) BOOL showsSelectionUsingAccessoryType;
 
+/**
+ Set and get the icon of the receiver. Aligned against the leading edge of the receiver.
+ 
+ The default is nil.
+ */
 @property (strong,nonatomic,nullable) UIImage *icon;
+/**
+ Set and get the title of the receiver. Aligned against the trailing edge of the icon. This supports multiline text.
+ 
+ The default is nil.
+ */
 @property (copy,nonatomic,nullable) NSString *title;
+/**
+ Set and get the subtitle of the receiver. Aligned against the trailing edge of the icon and bottom edge of the title. This supports multiline text.
+ 
+ The default is nil.
+ */
 @property (copy,nonatomic,nullable) NSString *subtitle;
+/**
+ Set and get the info of the receiver. Aligned against the trailing edge of the receiver.
+ 
+ The default is nil.
+ */
 @property (copy,nonatomic,nullable) NSString *info;
 
+/**
+ The tintColor to apply to the icon. Ensure your images are template images for them to render correctly. The defaults to the system returned value for tintColor.
+ 
+ The default is nil.
+ */
 @property (strong,nonatomic,nullable) UIColor *iconColor UI_APPEARANCE_SELECTOR;
+/**
+ The title text color.
+ 
+ The default is UIColor.blackColor.
+ */
 @property (strong,nonatomic,null_resettable) UIColor *titleColor UI_APPEARANCE_SELECTOR;
+/**
+ The subtitle text color.
+ 
+ The default is UIColor.darkGrayColor.
+ */
 @property (strong,nonatomic,null_resettable) UIColor *subtitleColor UI_APPEARANCE_SELECTOR;
+/**
+ The info text color.
+ 
+ The default is UIColor.lightGrayColor.
+ */
 @property (strong,nonatomic,null_resettable) UIColor *infoColor UI_APPEARANCE_SELECTOR;
 
+/**
+ The title text style to use when determining the title font.
+ 
+ The default is UIFontTextStyleBody.
+ */
 @property (copy,nonatomic,null_resettable) UIFontTextStyle titleTextStyle UI_APPEARANCE_SELECTOR;
+/**
+ The subtitle text style to use when determining the subtitle font.
+ 
+ The default is UIFontTextStyleFootnote.
+ */
 @property (copy,nonatomic,null_resettable) UIFontTextStyle subtitleTextStyle UI_APPEARANCE_SELECTOR;
+/**
+ The info text style to use when determining the info font.
+ 
+ The default is UIFontTextStyleFootnote.
+ */
 @property (copy,nonatomic,null_resettable) UIFontTextStyle infoTextStyle UI_APPEARANCE_SELECTOR;
 
+/**
+ Set and get the horizontal margin between subviews.
+ 
+ The default is 8.0.
+ */
 @property (assign,nonatomic) CGFloat horizontalMargin UI_APPEARANCE_SELECTOR;
+/**
+ Set and get the vertical margin between subviews.
+ 
+ The default is 4.0.
+ */
 @property (assign,nonatomic) CGFloat verticalMargin UI_APPEARANCE_SELECTOR;
 
+/**
+ Set and get the icon accessibility label. If this is nil, the icon will be hidden from the accessibility client.
+
+ The default is nil.
+ */
 @property (copy,nonatomic,nullable) NSString *iconAccessibilityLabel;
 
 @end
